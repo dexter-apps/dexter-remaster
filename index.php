4f21f20b258dbf632e06c2cee3df842c219546ca
dexter-apps <dexteros.lxde@gmail.com>
2026-08-24 23:02:43 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Principal</title>
+</head>
+<body>
+    <h1>Bienvenido a mi sitio web</h1>
+    
+    <?php
+        $mensaje = "Hola, este mensaje viene de PHP.";
+        echo "<p>$mensaje</p>";
+    ?>
+</body>
+</html>
